fb946c4aedf9fbab679e412b1be5ba2e71a9fc61
corgi-world <developer.msw@gmail.com>
2021-09-23 23:06:42 +0900

    합병 정렬 추가

--- 05-알고리즘과-파이썬/2주차/code/sortCheck.py
+++ 05-알고리즘과-파이썬/2주차/code/sortCheck.py
@@ -52,11 +52,38 @@ def quickSort(a, l, r):
         quickSort(a, l, i-1)
         quickSort(a, i+1, r)
 
+def mergeSort(a, l, r):
+    if r > l:
+        m = int((l+r)/2)
+        mergeSort(a, l, m)
+        mergeSort(a, m+1, r)
+        i, j, k = l, m+1, l
+        while i <= m and j <= r:
+            if a[i] < a[j]:
+                b[k] = a[i]
+                k += 1
+                i += 1
+            else:
+                b[k] = a[j]
+                k += 1
+                j += 1
+        if i > m:
+            for p in range(j, r+1):
+                b[k] = a[p]
+                k += 1
+        else:
+            for p in range(i, m+1):
+                b[k] = a[p]
+                k += 1
+        for p in range(l, r+1):
+            a[p] = b[p]
+
 ################################################
 
 import random, time
 import sys
-sys.setrecursionlimit(10000)
+sys.setrecursionlimit(20000)
+import copy
 
 def checkSort(a, n):
     isSorted = True
@@ -70,15 +97,16 @@ def checkSort(a, n):
     else:
         print("정렬 오류 발생")
 
-N = 300000
+N = 200000
 
 a = []
 a.append(None)
 for i in range(N):
     a.append(random.randint(1, N))
+b = copy.deepcopy(a)
 
 start_time = time.time()
-quickSort(a, 1, N)
+mergeSort(a, 1, N)
 end_time = time.time() - start_time
 print("선택 정렬의 실행 시간 (N=%d) : %0.3f"%(N, end_time))
 checkSort(a, N)
